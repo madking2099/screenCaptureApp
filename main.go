0069d69f9d377da16ff7c9deeafc4ec4654f9af9
No One <NoOne@NoWhere.ugh>
2025-02-26 19:26:27 -0600

    Enable CORS for Swagger UI

--- main.go
+++ main.go
@@ -32,6 +32,18 @@ func main() {
         log.Fatal(err)
     }
 
+    // Enable CORS for Swagger UI
+    r.Use(func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:1388")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+        c.Next()
+    })
+
     log.Println("Initializing routes")
     r.GET("/", redirectToSwagger)
     log.Println("Serving Swagger UI statically at /swagger/")
--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/api-docs/swagger.json",
+    url: "http://192.168.1.15:1388/api-docs/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
