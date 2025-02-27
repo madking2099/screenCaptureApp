64075a0ae80def4219c9dfc2be20c05ee861f673
No One <NoOne@NoWhere.ugh>
2025-02-26 20:22:33 -0600

    Fix routing conflict, use static /swagger for UI

--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -1,19 +1,13 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  // Get server host from query parameter or default to window.location.origin
-  const urlParams = new URLSearchParams(window.location.search);
-  const serverHost = urlParams.get('serverHost') || window.location.origin;
-  const url = serverHost + '/api-docs/swagger.json';
-  window.ui = SwaggerUIBundle({
-    url: url,
-    dom_id: '#swagger-ui',
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    layout: "BaseLayout",
-    deepLinking: true
-  });
-  //</editor-fold>
-};
+const urlParams = new URLSearchParams(window.location.search);
+const serverHost = urlParams.get('serverHost') || window.location.origin;
+const url = serverHost + '/api-docs/swagger.json';
+window.ui = SwaggerUIBundle({
+  url: url,
+  dom_id: '#swagger-ui',
+  presets: [
+    SwaggerUIBundle.presets.apis,
+    SwaggerUIStandalonePreset
+  ],
+  layout: "BaseLayout",
+  deepLinking: true
+});
